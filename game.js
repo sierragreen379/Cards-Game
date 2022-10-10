404139b7c34309bce9f67aa7b2150efdcf94ea76
Sierra Green <sierragreen379@gmail.com>
2022-10-10 00:17:56 -0600

    Added my own JS OOP and buttons to test it

--- game.js
+++ game.js
@@ -38,29 +38,68 @@ setInterval(() => {
 //   });
 // }
 
-// log(Deck.cards);
+// log(Deck);
 
-class Deck {
-  constructor() {
-    // values 1-9 here?
+let deck = [
+  {color: "blue", value: 1},
+  {color: "blue", value: 2},
+  {color: "blue", value: 3},
+  {color: "blue", value: 4},
+  {color: "blue", value: 5},
+  {color: "blue", value: 6},
+  {color: "blue", value: 7},
+  {color: "blue", value: 8},
+  {color: "blue", value: 9},
+  {color: "red", value: 1},
+  {color: "red", value: 2},
+  {color: "red", value: 3},
+  {color: "red", value: 4},
+  {color: "red", value: 5},
+  {color: "red", value: 6},
+  {color: "red", value: 7},
+  {color: "red", value: 8},
+  {color: "red", value: 9},
+  {color: "green", value: 1},
+  {color: "green", value: 2},
+  {color: "green", value: 3},
+  {color: "green", value: 4},
+  {color: "green", value: 5},
+  {color: "green", value: 6},
+  {color: "green", value: 7},
+  {color: "green", value: 8},
+  {color: "green", value: 9}
+];
+
+const testBotButton = document.getElementById("test-bot-button");
+const testPlayerButton = document.getElementById("test-player-button");
+
+class Card {
+  constructor({color, value} = cardType) {
+    this.color = color;
+    this.value = value;
+  }
+  setVisual() {
+    playingCard.style.backgroundColor = this.color;
+    playingCard.innerHTML = this.value;
   }
 }
 
-// Do we need these? Literally the only thing that will change is the color. We can use the array for that
-class Blue extends Deck {
-  constructor() {
+let playerHand = [];
+let botHand = [];
 
-  }
+function addToPlayerHand() {
+  let random = Math.floor(Math.random() * deck.length);
+  playerHand.push(new Card(deck[random]));
+  deck.splice(random, 1);
+  log("playerHand", playerHand);
 }
 
-class Red extends Deck {
-  constructor() {
-
-  }
+function addToBotHand() {
+  let random = Math.floor(Math.random() * deck.length);
+  botHand.push(new Card(deck[random]));
+  deck.splice(random, 1);
+  log("botHand", botHand);
 }
 
-class Green extends Deck {
-  constructor() {
-
-  }
-}
+testPlayerButton.onclick = addToPlayerHand;
+testBotButton.onclick = addToBotHand;
